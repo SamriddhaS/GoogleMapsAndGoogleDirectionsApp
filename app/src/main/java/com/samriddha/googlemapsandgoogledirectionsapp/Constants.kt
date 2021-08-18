7b14ea6a13c0d56f991b87771786abe21102c4e7
SamriddhaS <samriddha97@gamil.com>
2021-08-18 13:31:50 +0530

    check all requirements(play service,gps,location permission) for map

--- app/src/main/java/com/samriddha/googlemapsandgoogledirectionsapp/Constants.kt
+++ app/src/main/java/com/samriddha/googlemapsandgoogledirectionsapp/Constants.kt
@@ -1,6 +1,7 @@
-package com.samriddha.googlemapsandgoogledirectionsapp;
+package com.samriddha.googlemapsandgoogledirectionsapp
 
-public class Constants {
-
-    public static final int ERROR_DIALOG_REQUEST = 9001;
+object Constants {
+    const val ERROR_DIALOG_REQUEST = 9001
+    const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9002
+    const val PERMISSIONS_REQUEST_ENABLE_GPS = 9003
 }
--- app/src/main/java/com/samriddha/googlemapsandgoogledirectionsapp/ui/MainActivity.java
+++ app/src/main/java/com/samriddha/googlemapsandgoogledirectionsapp/ui/MainActivity.java
@@ -1,8 +1,16 @@
 package com.samriddha.googlemapsandgoogledirectionsapp.ui;
 
+import static com.samriddha.googlemapsandgoogledirectionsapp.Constants.ERROR_DIALOG_REQUEST;
+import static com.samriddha.googlemapsandgoogledirectionsapp.Constants.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
+import static com.samriddha.googlemapsandgoogledirectionsapp.Constants.PERMISSIONS_REQUEST_ENABLE_GPS;
+
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
@@ -15,9 +23,13 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
@@ -44,8 +56,7 @@ import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity implements
         View.OnClickListener,
-        ChatroomRecyclerAdapter.ChatroomRecyclerClickListener
-{
+        ChatroomRecyclerAdapter.ChatroomRecyclerClickListener {
 
     private static final String TAG = "MainActivity";
 
@@ -75,28 +86,28 @@ public class MainActivity extends AppCompatActivity implements
         initChatroomRecyclerView();
     }
 
-    private void initSupportActionBar(){
+    private void initSupportActionBar() {
         setTitle("Chatrooms");
     }
 
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
 
-            case R.id.fab_create_chatroom:{
+            case R.id.fab_create_chatroom: {
                 newChatroomDialog();
             }
         }
     }
 
-    private void initChatroomRecyclerView(){
+    private void initChatroomRecyclerView() {
         mChatroomRecyclerAdapter = new ChatroomRecyclerAdapter(mChatrooms, this);
         mChatroomRecyclerView.setAdapter(mChatroomRecyclerAdapter);
         mChatroomRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    private void getChatrooms(){
+    private void getChatrooms() {
 
         FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
                 .build();
@@ -115,11 +126,11 @@ public class MainActivity extends AppCompatActivity implements
                     return;
                 }
 
-                if(queryDocumentSnapshots != null){
+                if (queryDocumentSnapshots != null) {
                     for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
 
                         Chatroom chatroom = doc.toObject(Chatroom.class);
-                        if(!mChatroomIds.contains(chatroom.getChatroom_id())){
+                        if (!mChatroomIds.contains(chatroom.getChatroom_id())) {
                             mChatroomIds.add(chatroom.getChatroom_id());
                             mChatrooms.add(chatroom);
                         }
@@ -132,7 +143,7 @@ public class MainActivity extends AppCompatActivity implements
         });
     }
 
-    private void buildNewChatroom(String chatroomName){
+    private void buildNewChatroom(String chatroomName) {
 
         final Chatroom chatroom = new Chatroom();
         chatroom.setTitle(chatroomName);
@@ -152,9 +163,9 @@ public class MainActivity extends AppCompatActivity implements
             public void onComplete(@NonNull Task<Void> task) {
                 hideDialog();
 
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     navChatroomActivity(chatroom);
-                }else{
+                } else {
                     View parentLayout = findViewById(android.R.id.content);
                     Snackbar.make(parentLayout, "Something went wrong.", Snackbar.LENGTH_SHORT).show();
                 }
@@ -162,13 +173,13 @@ public class MainActivity extends AppCompatActivity implements
         });
     }
 
-    private void navChatroomActivity(Chatroom chatroom){
+    private void navChatroomActivity(Chatroom chatroom) {
         Intent intent = new Intent(MainActivity.this, ChatroomActivity.class);
         intent.putExtra(getString(R.string.intent_chatroom), chatroom);
         startActivity(intent);
     }
 
-    private void newChatroomDialog(){
+    private void newChatroomDialog() {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Enter a chatroom name");
@@ -180,10 +191,9 @@ public class MainActivity extends AppCompatActivity implements
         builder.setPositiveButton("CREATE", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                if(!input.getText().toString().equals("")){
+                if (!input.getText().toString().equals("")) {
                     buildNewChatroom(input.getText().toString());
-                }
-                else {
+                } else {
                     Toast.makeText(MainActivity.this, "Enter a chatroom name", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -201,7 +211,7 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(mChatroomEventListener != null){
+        if (mChatroomEventListener != null) {
             mChatroomEventListener.remove();
         }
     }
@@ -209,15 +219,156 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     protected void onResume() {
         super.onResume();
-        getChatrooms();
+        Log.d(TAG,"onResumed");
+        if (checkAllRequirementsForMap()) {
+            getChatrooms();
+        }
     }
+
+    private boolean checkAllRequirementsForMap() {
+        /*
+         * First check if the device has google play services installed.
+         * Then check if user has enabled the gps.
+         * Then check if user has given location permission
+         * */
+        if (isPlayServiceAvailable()) {
+            if (isGpsEnabled()) {
+                if (hasLocationPermission()) {
+                    return true;
+                } else {
+                    askLocationPermission();
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isPlayServiceAvailable() {
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if (available == ConnectionResult.SUCCESS) {
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Google play service not available", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    public boolean isGpsEnabled() {
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+            return false;
+        }
+        return true;
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("This application requires GPS to work properly, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", (dialog, id) -> {
+                    Intent enableGpsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(enableGpsIntent);
+                    /*
+                     * This part can be used to check if the user enabled gps in the settings or not and take
+                     * action accordingly but we don't need it here as we are check for the gps inside onResume.
+                     * */
+                    //startActivityForResult(enableGpsIntent, PERMISSIONS_REQUEST_ENABLE_GPS);
+
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: called.");
+        /*
+         * This part can be used to check if the user enabled gps in the settings or not and take
+         * action accordingly but we don't need it here as we are check for the gps inside onResume.
+         * */
+        if (requestCode == RESULT_OK) {
+            switch (requestCode) {
+                case PERMISSIONS_REQUEST_ENABLE_GPS: {
+                    if (checkAllRequirementsForMap()) {
+                        getChatrooms();
+                    }
+                }
+            }
+        }
+
+    }
+
+    private boolean hasLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void askLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        /*
+         * This part can be used to check if the user granted the permissions or not and take
+         * action accordingly but we don't need it here as we are check for the permissions inside onResume.
+         * */
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG,"Permission granted");
+                    getChatrooms();
+                }else{
+                    Toast.makeText(MainActivity.this, "Please grant location permission", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
 
     @Override
     public void onChatroomSelected(int position) {
         navChatroomActivity(mChatrooms.get(position));
     }
 
-    private void signOut(){
+    private void signOut() {
         FirebaseAuth.getInstance().signOut();
         Intent intent = new Intent(this, LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -234,27 +385,27 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
-            case R.id.action_sign_out:{
+        switch (item.getItemId()) {
+            case R.id.action_sign_out: {
                 signOut();
                 return true;
             }
-            case R.id.action_profile:{
+            case R.id.action_profile: {
                 startActivity(new Intent(this, ProfileActivity.class));
                 return true;
             }
-            default:{
+            default: {
                 return super.onOptionsItemSelected(item);
             }
         }
 
     }
 
-    private void showDialog(){
+    private void showDialog() {
         mProgressBar.setVisibility(View.VISIBLE);
     }
 
-    private void hideDialog(){
+    private void hideDialog() {
         mProgressBar.setVisibility(View.GONE);
     }
 
